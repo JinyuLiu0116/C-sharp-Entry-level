66aeb9e8ae9a873437683fce1ed53f0c2efa3f81
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-09-12 16:41:51 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿string greeting = "Hello World! My name is Jinyu";
+Console.WriteLine(greeting);
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.Clear();
+
+Console.WriteLine("***************************************************");
+Console.WriteLine("**     Programming Assignment: #2                **");
+Console.WriteLine("**     Developer: Jinyu Liu                      **");
+Console.WriteLine("**     Date Submitted: September 12              **");
+Console.WriteLine("**     Purpose: Provide internal documentation   **");
+Console.WriteLine("***************************************************");
+
+Console.ResetColor();
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.Clear();
+
+Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine("| *******-------------------------------------------|");
+Console.WriteLine("| *******-------------------------------------------|");
+Console.WriteLine("| *******-------------------------------------------|");
+Console.WriteLine("| *******-------------------------------------------|");
+Console.WriteLine("|---------------------------------------------------|");
+Console.WriteLine("|---------------------------------------------------|");
+Console.WriteLine("-----------------------------------------------------");
+
+Console.ResetColor();
+
