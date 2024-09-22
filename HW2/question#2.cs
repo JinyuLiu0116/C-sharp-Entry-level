59924b37438fa0334bf18d0240083f484073ddf2
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-09-22 14:42:36 -0400

    Update question#2.cs

--- HW2/question#2.cs
+++ HW2/question#2.cs
@@ -1,1 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace HW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dMiles;
+            double dKilometers;
+            const double converToKilometers = 1.609344;
+            int iFeet;
+            const int converToFeet = 5280;
+            while (true)
+            {
+                string userInput;
+                Console.Write("Enter a distance as Miles(-1 to exit):");
+                userInput = Console.ReadLine();
+                if (userInput == "-1") {
+                    Console.WriteLine("Thank you!");
+                    break;
+                }
+                if (!double.TryParse(userInput, out dMiles))
+                {
+                    Console.WriteLine("Invalid input");
+                    continue;
+                }
+                else if (dMiles < 0)
+                {
+                    Console.WriteLine("Distance cannot be negative");
+                }
+                else
+                {
+                    dKilometers = Math.Round(dMiles * converToKilometers, 2);
+                    iFeet = (int)Math.Round(dMiles * converToFeet);
+
+                    Console.WriteLine("Miles: " + Math.Round(dMiles, 2));
+                    Console.WriteLine("Kilometers: " + dKilometers);
+                    Console.WriteLine("Feet: " + iFeet);
+                }
+            }
+    
+            Console.ReadKey();
+        }
+    }
+}
