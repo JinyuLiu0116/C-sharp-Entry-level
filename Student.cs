100af07c588f99f913a6f8feeb64c123d53e86cc
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-09-26 18:44:54 -0400

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace execrise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentApp oneStudent = new StudentApp("0001234567", "Joan", "Logan",
+                                             "Senior", "English");
+            Console.WriteLine("First Test:");
+            DisplayInfo(oneStudent);
+            Console.WriteLine();
+            Console.WriteLine("Another Test:");
+            Console.WriteLine(oneStudent.ToString());
+
+            Console.ReadKey();
+        }
+        static void DisplayInfo(StudentApp aStudent)
+        {
+            Console.WriteLine($"Name: {aStudent.ReturnFullName()}");
+            Console.WriteLine($"Major: {aStudent.getMajor()}");
+        }
+    }
+}
