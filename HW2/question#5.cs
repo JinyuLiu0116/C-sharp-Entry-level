9d114cd0db9446e88dffa8a15681d3d2191f6055
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-10-10 15:59:53 -0400

    Update question#5.cs

--- HW2/question#5.cs
+++ HW2/question#5.cs
@@ -10,25 +10,25 @@ namespace HW2Q9
     {
         static void Main(string[] args)
         {
-            const string product1 = "French fries";
-            const string product2 = "Haricot verts";
-            const double productOnePerHGrams = 1.29, productTwoPerHGrams = 0.75;
-            const double gramToPound = 0.00220462;
+            const string PRODUCT_1 = "French fries";
+            const string PRODUCT_2 = "Haricot verts";
+            const double PRODUCT_1_PER_GRAM = 1.29, PRODUCT_2_PER_GRAM = 0.75;
+            const double GRAM_CONVER_TO_POUND = 0.00220462;
             int weightInGrams = 100;
-            double weightInPounds = weightInGrams * gramToPound;
-            double totalPriceOfProductOne = productOnePerHGrams * weightInGrams;
-            double totalPriceOfProductTwo = productTwoPerHGrams * weightInGrams;
+            double weightInPounds = weightInGrams * GRAM_CONVER_TO_POUND;
+            double totalPriceOfProductOne = PRODUCT_1_PER_GRAM * weightInGrams;
+            double totalPriceOfProductTwo = PRODUCT_2_PER_GRAM * weightInGrams;
             double productOnePerHPounds = (totalPriceOfProductOne / weightInPounds) / 100;
             double productTwoPerHPounds = (totalPriceOfProductTwo / weightInPounds) / 100;
 
-            Console.WriteLine("Product name: ----------- " + product1);
-            Console.WriteLine("Price per 100 Grams: --- $" + productOnePerHGrams);
+            Console.WriteLine("Product name: ----------- " + PRODUCT_1);
+            Console.WriteLine("Price per 100 Grams: --- $" + PRODUCT_1_PE_GRAM);
             Console.WriteLine("Weight in Grams: -------- " + weightInGrams + "g");
             Console.WriteLine("Weight in Pounds: ------- " + Math.Round(weightInPounds, 2) + " lbs");
             Console.WriteLine("Price per Pound: ------- $" + Math.Round(productOnePerHPounds, 2));
             Console.WriteLine();
-            Console.WriteLine("Product name: ---------- " + product2);
-            Console.WriteLine("Price per 100 Grams: --- $" + productTwoPerHGrams);
+            Console.WriteLine("Product name: ---------- " + PRODUCT_2);
+            Console.WriteLine("Price per 100 Grams: --- $" + PRODUCT_2_PER_GRAM);
             Console.WriteLine("Weight in Grams: ------- " + weightInGrams + "g");
             Console.WriteLine("Weight in Pounds: ------ " + Math.Round(weightInPounds, 2) + " lbs");
             Console.WriteLine("Price per Pound: ------- $" + Math.Round(productTwoPerHPounds, 2));
