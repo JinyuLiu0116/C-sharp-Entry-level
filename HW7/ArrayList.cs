98aa20a3802756d893ce9a7d5aac6dd7cad08b73
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-11-22 19:33:18 -0500

    Update ArrayList.cs

--- HW7/ArrayList.cs
+++ HW7/ArrayList.cs
@@ -1,1 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7
+{
+
+    internal class Program
+    {
+        static Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            ArrayList alFruits = new ArrayList();
+
+            alFruits.Add("Peach");
+            alFruits.Add("Watermelon");
+            alFruits.Add("Pum");
+
+            Console.Write("Fruits: ");
+            for (int i = 0; i < alFruits.Count; i++) 
+            {
+                Console.Write(alFruits[i] + " ");
+            }
+            Console.WriteLine();
+
+            alFruits.Add("Apple");
+            alFruits.Add("Bluebarry");
+            alFruits.Remove("Pum");
+
+            alFruits.Sort();
+            Console.Write("Fruits: ");
+            DisplayArrayList(alFruits);
+
+
+            Console.ReadKey();
+        }
+        static void DisplayArrayList(ArrayList Fruits)
+        {
+            foreach(string fruit in Fruits)
+            {
+                Console.Write(fruit + " ");
+            }
+        }
+
+    }
+}
 
